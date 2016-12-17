d0409a99ea2a647ee81e111f699fad0ee2d8819c
bweigel <bweigel@ipb-halle.de>
2016-12-18 00:34:23 +0100

    [Spark] Spark access to S3 is working. Readme.md for requirements

--- src/main/scala/algorithms/fibonacci.scala
+++ src/main/scala/algorithms/fibonacci.scala
@@ -3,9 +3,9 @@ package algorithms
 object fibonacci {
   def fib(n: Int): BigInt = {
     @annotation.tailrec
-    def loop(togo: Int, current: BigInt = 1, acc: BigInt = 0): BigInt = {
-      if(togo == 1) acc
-      else loop(togo-1, acc+current, current)
+    def loop(togo: Int, nextNumber: BigInt = 1, accumulator: BigInt = 0): BigInt = {
+      if(togo == 1) accumulator
+      else loop(togo-1, accumulator+nextNumber, nextNumber)
     }
     loop(n)
   }
--- src/main/scala/spark/spark.scala
+++ src/main/scala/spark/spark.scala
@@ -4,16 +4,20 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object spark {
   def main(args: Array[String]) {
-    //val logFile = "/home/bweigel/Public/README.md" // Should be some file on your system
-    val logFile = "s3n://europace.reporting/test/raw/Bausteine_VGD43.csv"
-    // Should be some file on your system
-    val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
+
+    val logFile = "s3a://elasticmapreduce/samples/wordcount/wordSplitter.py"
+
+    val conf = new SparkConf().setAppName("Simple Application")
+      .setMaster("local")
+
     val sc = new SparkContext(conf)
-    //sc.hadoopConfiguration.set()
+
     val logData = sc.textFile(logFile).cache()
+
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
     println(s"Lines with a: $numAs, Lines with b: $numBs")
+
     sc.stop()
   }
 }
